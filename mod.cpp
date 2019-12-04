9efc6f493bbba73150702e187249d7846c32b776
Sondre Bjørnvold Bakken <Sondrebakken@gmail.com>
2019-12-04 19:22:40 +0100

    Added mod.cpp

---
+++ mod.cpp
@@ -0,0 +1,11 @@
+
+name = "Arma.warfare 2.0.0";  //This name will be displayed only in game , specifically the expansion menu
+picture = "logo_warfare.paa"; //Location of your addon's icon
+logo = "logo_warfare.paa";
+logoOver = "logo_warfare.paa";
+actionName = "Website";                                    //The name of the action
+action = "https://github.com/KillahPotatoes/arma.warfare"; //The action , "Authors Website Link"
+description = "Arma.warfare - Version 2.0.0";              // Addon Description
+overview = "Capture the sector mission";
+author = "Sondrebbakken";
+overviewPicture = "logo_warfare.paa";

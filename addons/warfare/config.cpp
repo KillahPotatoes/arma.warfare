6abec4a6b8e8223fd723a9a31464e192ddee1f8e
Sondre Bjørnvold Bakken <Sondrebakken@gmail.com>
2019-12-04 19:22:34 +0100

    Added config.cpp

---
+++ addons/warfare/config.cpp
@@ -0,0 +1,17 @@
+class CfgMissions
+{
+	class Campaigns
+	{
+		class Overthrow
+		{
+			directory = "warfare\campaign";
+		};
+	};
+	class MPMissions
+	{
+		class OverthrowMpTanoa
+		{
+			directory = "warfare\campaign\missions\stratis";
+		};
+	};
+};
